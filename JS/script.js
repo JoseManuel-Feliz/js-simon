04d3874465a8051cfbac236cf4c2b9d5e302634d
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-28 05:24:26 +0200

    added DOM elements & JS steps

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+console.log('JS OK')
+
+//*STEPS
+
+/* 
+
+//* 1. Recuperare gli elementi d'interesse all'interno del DOM.
+
+//* 2. Creare un array che conterra i numeri da mostrare all'inizio del countdown.
+
+//* 2. Creare un array che conterra il value degli input number.
+
+
+//* 4. creare un evento al submit del form.
+    ? 4b. impostare un intervallo di 1000 ms.
+    ? 4c.  per ogni secondo che passa decrementare il nr del countdown.
+    ? 4d.  creare una condizione che verifichi se il valore del countdown è uguale a zero.
+        ! se è zero { rimuovere l'intervallo, svuotare il countdown, cambiare il testo a request message, 
+        !            dare class block a gli input number e d-none ai nr iniziali } 
+    
+    ? 4e. Recuperare il value degli input text e inserirli nel array dei value input.
+    ? 4f. creare una condizione che verifichi se il value degli input è incluso nel array dei nr casuali.
+        ! stampare un messaggio appropiato in caso che siano presenti o meno.
+
+
+
+*/
