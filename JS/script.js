ca8cbbcc47ad1252a3d310ef78a8725c6058c736
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-28 09:59:56 +0200

    added interval, added form eventlistener, added btn play eventlistener

--- JS/script.js
+++ JS/script.js
@@ -24,14 +24,17 @@ console.log('JS OK')
 
 */
 
+//? Fase di prerazione
 
 //* 1. Recuperare gli elementi d'interesse all'interno del DOM.
 
 const simonCountDown = document.getElementById('count-down')
 const requestMessage = document.getElementById('request-message')
+const replyMessage = document.getElementById('reply-message')
 const form = document.querySelector('form')
 const guessNum = document.querySelectorAll('span')
 const enterNum = document.querySelectorAll('input')
+const checkBtn = document.getElementById('check')
 const playBtn = document.getElementById('play')
 
 console.log(guessNum, enterNum[0])
@@ -46,3 +49,55 @@ for (i = 0; i < guessNum.length; i++) {
     console.log(guessNum[i].innerText)
 }
 
+//* 3. Creare un array che conterra il value degli input number.
+let numEntered = []
+
+simonCountDown.innerText = 'Simon Says'
+//? Fase gestione degli eventi
+
+//* 4. creare un evento al submit del form.
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    for (let i = 0; i < enterNum.length; i++) {
+        numEntered.push(parseInt(enterNum[i].value))
+
+    }
+    console.log(numEntered)
+    if (numEntered === numbersToGuess) {
+        replyMessage.innerText = 'Hai indovinato tutti i numeri'
+    }
+    console.log(numEntered, numbersToGuess, replyMessage)
+})
+
+playBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    //nascondo il btn play
+    playBtn.classList.add('d-none')
+    //rimuovo le classi d-none sugli elementi che mi interessano
+    requestMessage.classList.remove('d-none')
+
+    let seconds = 5;
+    requestMessage.innerText = 'Memorizza i numeri entro il tempo limite!'
+
+    // 4b. impostare un intervallo di 1000 ms.
+    const count = setInterval(() => {
+
+        // 4c.  per ogni secondo che passa decrementare il nr del countdown.
+        simonCountDown.innerHTML = --seconds
+        // 4d.  creare una condizione che verifichi se il valore del countdown è uguale a zero.
+        form.classList.remove('d-none')
+        if (seconds === 0) {
+            clearInterval(count)
+            simonCountDown.innerText = '-';
+            requestMessage.innerText = "Inserisci tutti i numeri che ricordi (l'ordine non è importante)"
+
+            for (i = 0; i < guessNum.length; i++) {
+                guessNum[i].classList.add('d-none')
+                enterNum[i].classList.remove('d-none')
+            }
+        }
+    }, 1000);
+
+
+})
